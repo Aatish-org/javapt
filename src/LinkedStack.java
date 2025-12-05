06b80b8e8ec2d08438342a9e6506f025a2d01261
Aatish-org <aatishoffic@gmail.com>
2025-12-05 12:22:18 +0530

    Linked Stack

--- src/LinkedStack.java
+++ src/LinkedStack.java
@@ -1,10 +1,42 @@
 public class LinkedStack {
-    Class Node {
+    class Node {
         int data;
         Node next;
         Node(int data) {
             this.data = data;
             this.next = null;
         }
+    }
+
+    Node top;
+//    LStack(){top=null;}
+
+    void push(int data){
+        Node n=new Node(data);
+        if(top==null){top=n;}
+        else{
+            n.next=top;
+            top=n;
+        }
+    }
+
+    void pop(){
+        if(top==null){System.out.println("LinkedStack is Empty");}
+        else{
+            top.next=top;
+        }
+    }
+
+    void peek(){
+        if(top==null){System.out.println("LinkedStack is Empty");}
+        else{
+            System.out.println("Top Element is"+top);
+        }
+    }
+
+
+    public static void main(String[] arg){
+        
+
     }
 }
