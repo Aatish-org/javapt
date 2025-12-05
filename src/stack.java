efc662d8bcc1482d2e3c9e2504f3632c270aa38c
Aatish-org <aatishoffic@gmail.com>
2025-12-05 11:59:52 +0530

    Linked Stack

--- src/stack.java
+++ src/stack.java
@@ -1,6 +1,8 @@
+import java.util.Stack;
+
 public class stack {
     int top,size,arr[];
-    void Stack(int size){
+    int Stack(int size){
         this.size=size;
         arr=new int[size];
         top=-1;
@@ -15,7 +17,37 @@ public class stack {
             arr[top]=data;
         }
     }
+
+    void pop(){
+        if(top==-1){System.out.println("Stack is Empty");}
+        else{
+            top--;
+        }
+    }
+
+    void peek(){
+        if(top==-1){System.out.println("Stack is Empty");}
+        else{
+            System.out.println("Top element is: "+arr[top]);
+        }
+    }
+    void display(){
+        if(top==-1){System.out.println("Stack is Empty");}
+        else{
+            for(int i=top;i>=0;i--){
+                System.out.println(arr[i]);
+            }
+        }
+    }
     public static void main(String[] args) {
+        Stack st=new Stack();
+        st.push(5);
+        st.push(10);
+        st.push(15);
+        st.display();
+        st.peek();
+        st.pop();
+        st.display();
 
     }
 }
